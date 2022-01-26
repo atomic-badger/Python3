1521b69ad32e2705ba415065faff33ca5342c835
milburn brock <96309943+atomic-badger@users.noreply.github.com>
2022-01-26 15:20:17 -0500

    Add files via upload

---
+++ keywordslist.py
@@ -0,0 +1,12 @@
+# The following script lists keywords in python
+
+print("\nThe following are Python 3 keywords. Do not use these without cause.\n")
+print("and\t\tdel\t\tfrom\t\tNone\t\tTrue")
+print("as\t\telif\t\tglobal\t\tnonlocal\ttry")
+print("assert\t\telse\t\tif\t\tnot\t\twhile")
+print("break\t\texcept\t\timport\t\tor\t\twith")
+print("class\t\tFalse\t\tin\t\tpass\t\tyield")
+print("continue\tfinally\t\tis\t\traise\t\tasync")
+print("def\t\tfor\t\tlambda\t\treturn\t\tawait")
+
+print("\n(from Python for Everybody by Charles Severance, page 21.)\n")
