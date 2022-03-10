9202a880ea9b5635bb0d948d8ecbf8412048fa98
milburn brock <96309943+atomic-badger@users.noreply.github.com>
2022-03-09 20:27:31 -0500

    Add files via upload

---
+++ emerald_tablet.py
@@ -0,0 +1,22 @@
+print("\nTHE EMERALD TABLET OF HERMES: 21ST CENTURY TRANSLATION.\n")
+
+print("\n1.\tTruth. Certainty in the Cosmos. This is what should be observed.\n")
+
+print("\n2.\tThat which is in the world within is like that which is in the world without,")
+print("\t\tmaking the manifestions of the Overmind.\n")
+
+print("\n3.\tAs all Reality is made manifest, issuing forth from the essence of the Overmind by self-evolution.\n")
+
+print("\n4.\tIts Initiator is the Solar force. Its caregiver is the Lunar force.\n")
+
+print("\n5.\tManifestation carried it in its center, Force of Movement grew it in its center.\n")
+
+print("\n6.\tAs the Manifest will become Essence.\n")
+
+print("\n7.\tForm the Manifest with Essence, using the greatest energy.\n")
+
+print("\n8.\tThe evolving force ascends from the manifest to essence, making it the Initiator of the world within and the world without.\n")
+
+print("\n9.\tThus is the code of of the Cosmos explained.\n")
+
+
